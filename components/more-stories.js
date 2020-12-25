ebe008cef241dd0a0e43bb92597e912daaa01bb3
Clara <hey@clarale.com>
2020-12-25 16:53:10 +0100

    Remove post grid and edit post-preview

--- components/more-stories.js
+++ components/more-stories.js
@@ -6,14 +6,14 @@ export default function MoreStories({ posts }) {
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         Here's What's New
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
+      <div>
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
             title={post.title}
             // coverImage={post.coverImage}
             date={post.date}
-            author={post.author}
+            // author={post.author}
             slug={post.slug}
             excerpt={post.excerpt}
           />
--- components/post-preview.js
+++ components/post-preview.js
@@ -16,12 +16,12 @@ export default function PostPreview({
       {/* <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div> */}
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className="text-3xl mb-2 leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline">{title}</a>
         </Link>
       </h3>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+      <p className="text-lg leading-normal mb-4">{excerpt}</p>
       <div className="text-lg mb-4">
         <DateFormatter dateString={date} />
       </div>
