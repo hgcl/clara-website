cb6886e5591d0ebddcb552628bafec318d66d3b5
Clara <hey@clarale.com>
2020-12-25 16:17:44 +0100

    Remove HOME_OG_IMAGE_URL constant

--- components/meta.js
+++ components/meta.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-// import { CMS_NAME } from '../lib/constants'
-import { HOME_OG_IMAGE_URL } from '../lib/constants'
+// import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 
 export default function Meta() {
   return (
@@ -36,8 +35,8 @@ export default function Meta() {
       {/* <meta
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      /> */}
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      /> 
+      <meta property="og:image" content={HOME_OG_IMAGE_URL} />*/}
     </Head>
   )
 }
