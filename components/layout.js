5d9d9921415d90e7a94744bd53e35bc1c53956c9
Clara <hey@clarale.com>
2020-12-27 00:25:46 +0100

    Add menu

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
 import Meta from '../components/meta'
+import Nav from '../components/nav'
 
 // Alert at page top + footer
 
@@ -10,6 +11,7 @@ export default function Layout({ preview, children }) {
       <Meta />
       <div className="min-h-screen">
         <Alert preview={preview} />
+        <Nav />
         <main>{children}</main>
       </div>
       <Footer />
