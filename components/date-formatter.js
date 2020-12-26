afbdcbaa463d606fef6a4dced85aa386d8b4d442
Clara <hey@clarale.com>
2020-12-26 14:53:29 +0100

    Edit h-entry property: dt-published

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function DateFormatter({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return <time className="dt-published" dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
 }
--- components/post-header.js
+++ components/post-header.js
@@ -17,7 +17,7 @@ export default function PostHeader({ title, coverImage, date, author }) {
         {/* <div className="block md:hidden mb-6">
           <Avatar name={author.name} picture={author.picture} />
         </div> */}
-        <div className="dt-published mb-6 text-lg">
+        <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
         </div>
       </div>
