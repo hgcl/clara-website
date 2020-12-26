15609c137e6d316a16f99f6c43494e72b2ebc148
Clara <hey@clarale.com>
2020-12-26 16:30:14 +0100

    Add Link component

---
+++ components/link.js
@@ -0,0 +1,13 @@
+import NextLink from 'next/link'
+
+export default function Link({ href, as, children, className }) {
+  const isExternal = href.startsWith("http")
+  if (isExternal) {
+      return (<a className={`underline ${className}`} href={href}>{children}</a>)
+  }
+  return (
+    <NextLink href={href} as={as}>
+      <a className={`underline ${className}`}>{ children }</a>
+    </NextLink>
+  )
+}
