25bc9a028169c4217ab115e99451d49d6b747ee2
Clara <hey@clarale.com>
2020-12-27 23:12:54 +0100

    Capitalize LinkTitle

--- components/LinkTitle.js
+++ components/LinkTitle.js
@@ -6,14 +6,14 @@ export default function Link({ href, as, children, className }) {
   const isExternal = href.startsWith("http");
   if (isExternal) {
     return (
-      <a className={`${className}`} href={href}>
+      <a className={`capitalize ${className}`} href={href}>
         {children}
       </a>
     );
   }
   return (
     <NextLink href={href} as={as}>
-      <a className={`${className}`}>{children}</a>
+      <a className={`capitalize ${className}`}>{children}</a>
     </NextLink>
   );
 }
