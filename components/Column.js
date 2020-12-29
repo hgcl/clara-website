2e4dc6b5005edf257767983854f93b38f0802448
Clara <hey@clarale.com>
2020-12-29 19:56:56 +0100

    Remove Column border

--- components/Column.js
+++ components/Column.js
@@ -1,6 +1,6 @@
 export default function Column({ children, size }) {
   return (
-    <div className={`flex-${size} border-solid border-2 border-white`}>
+    <div className={`flex-${size}`}>
       {children}
     </div>
   );
