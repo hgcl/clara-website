e77ce6aeb25049c96d46c5c23cf28fbfe31bf8ab
Clara <hey@clarale.com>
2020-12-30 17:31:26 +0100

    Fix typo in path

--- components/post-header.js
+++ components/post-header.js
@@ -1,5 +1,5 @@
 // import Avatar from './avatar';
-import DateFormatter from "../date-formatter";
+import DateFormatter from "./date-formatter";
 // import CoverImage from './cover-image'
 import PostTitle from "./post-title";
 
