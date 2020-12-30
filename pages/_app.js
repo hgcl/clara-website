9b5485f2c871cfeacdba09c11299530590d34413
Clara <hey@clarale.com>
2020-12-30 17:20:29 +0100

    Import necessary fonts only

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,12 @@
+// Typeface: Vesper Libre
+import "fontsource-vesper-libre/400.css";
+
+// Typeface: Source Sans Pro
+import "fontsource-source-sans-pro/400.css";
+import "fontsource-source-sans-pro/400-italic.css";
+import "fontsource-source-sans-pro/700.css";
+import "fontsource-source-sans-pro/700-italic.css";
+
 import "fontsource-vesper-libre";
 import "fontsource-source-sans-pro";
 import "../styles/index.css";
