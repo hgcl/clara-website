be4bf1318bfaf68b94775c622c9f89751b1b25a2
Clara <hey@clarale.com>
2021-01-01 17:37:24 +0100

    Remove unnecessary font imports

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,6 @@ import "fontsource-source-sans-pro/400-italic.css";
 import "fontsource-source-sans-pro/700.css";
 import "fontsource-source-sans-pro/700-italic.css";
 
-import "fontsource-vesper-libre";
-import "fontsource-source-sans-pro";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }) {
