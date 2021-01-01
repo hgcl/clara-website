2d6f0916d9334d8eec75db371673882ecb0141fb
Clara <hey@clarale.com>
2021-01-01 18:36:30 +0100

    Remove PostBody from [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,7 +2,7 @@
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 import Container from "../../components/container";
-import PostBody from "../../components/post-body";
+// import PostBody from "../../components/post-body";
 import Header from "../../components/header";
 import PostHeader from "../../components/post-header";
 import Layout from "../../components/layout";
