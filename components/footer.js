5906735d61fbc6886a94bb7c559f8021044ad563
Clara <hey@clarale.com>
2021-01-01 16:46:03 +0100

    Fix icon style bug

--- components/footer.js
+++ components/footer.js
@@ -7,18 +7,19 @@ import IconTwitter from "../public/icons/IconTwitter";
 import IconEtsy from "../public/icons/IconEtsy";
 
 export default function Footer() {
+  const iconStyle = `mx-2 fill-current`
   return (
     <footer className="bg-gray-dark h-40">
       <div className="flex flex-col items-center text-gray-regular h-full justify-end mb-16">
         <div className="flex flex-row items-center mb-8">
-          <span className="mx-2">
+          <span className={iconStyle}>
             <Link href="https://twitter.com/clara__le">
-              <IconTwitter />
+              <IconTwitter height="h-4"/>
             </Link>
           </span>
-          <span className="mx-2">
+          <span className={iconStyle}>
             <Link href="https://lifeundercontrol.etsy.com">
-              <IconEtsy />
+              <IconEtsy height="h-4"/>
             </Link>
           </span>
         </div>
