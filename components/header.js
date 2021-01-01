af7c79c4528242a2ad6d8e48790966c2c8a46707
Clara <hey@clarale.com>
2021-01-01 19:23:26 +0100

    Remove underline on hover of header link

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-        <a className="hover:underline">{WEBSITE_NAME}</a>
+        <a>{WEBSITE_NAME}</a>
       </Link>
     </h2>
   );
