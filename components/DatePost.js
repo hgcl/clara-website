e6ae567dcc9794f7e9447b06df38701d0f216d56
Clara <hey@clarale.com>
2021-01-01 17:22:46 +0100

    Run Prettier and remove superficial file

--- components/DatePost.js
+++ components/DatePost.js
@@ -3,7 +3,8 @@ import DateFormatter from "./date-formatter";
 export default function DatePost({ dateString, lastUpdated }) {
   return (
     <div className="text-sm tracking-widest uppercase mb-8">
-      {lastUpdated ? 'Last updated on' : 'Published on'} <DateFormatter dateString={dateString} />
+      {lastUpdated ? "Last updated on" : "Published on"}{" "}
+      <DateFormatter dateString={dateString} />
     </div>
   );
 }
