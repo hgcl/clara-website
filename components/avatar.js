4a84de049960664f01e72807e59c0a113e5a8be8
Clara <hey@clarale.com>
2021-01-05 17:50:04 +0100

    Remove font-medium for all headings

--- components/avatar.js
+++ components/avatar.js
@@ -2,7 +2,7 @@ export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
       <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
-      <div className="text-lg font-medium">{name}</div>
+      <div className="text-lg">{name}</div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // Typeface: Vesper Libre
 import "fontsource-vesper-libre/400.css";
-import "fontsource-vesper-libre/500.css";
 
 // Typeface: Source Sans Pro
 import "fontsource-source-sans-pro/400.css";
