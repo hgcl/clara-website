daf337e86bff605f428fb47ac7c8a1111ef77c60
Clara <hey@clarale.com>
2021-01-05 14:26:30 +0100

    Fix icon color fill

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,3 @@
-import Container from "./container";
-import { EXAMPLE_PATH } from "../lib/constants";
 import Link from "./link";
 
 // icons
@@ -9,29 +7,31 @@ import IconRss from "../public/icons/IconRss";
 import IconGithub from "../public/icons/IconGithub";
 
 export default function Footer() {
-  const iconStyle = `mx-2 fill-current`;
+  const iconStyle = "mx-2";
+  const iconHeight = "h-4";
+  const iconFill = "fill-current text-gray-regular";
   return (
-    <footer className="bg-gray-dark h-40">
+    <footer className="bg-gray-dark h-40 mb-16">
       <div className="flex flex-col items-center text-gray-regular h-full justify-end mb-16">
         <div className="flex flex-row items-center mb-8">
           <span className={iconStyle}>
             <Link href="https://twitter.com/clara__le">
-              <IconTwitter height="h-4" />
+              <IconTwitter height={iconHeight} fill={iconFill} />
             </Link>
           </span>
           <span className={iconStyle}>
             <Link href="https://lifeundercontrol.etsy.com">
-              <IconEtsy height="h-4" />
+              <IconEtsy height={iconHeight} fill={iconFill} />
             </Link>
           </span>
           <span className={iconStyle}>
             <Link href="https://github.com/hgcl/">
-              <IconGithub height="h-4" />
+              <IconGithub height={iconHeight} fill={iconFill} />
             </Link>
           </span>
           <span className={iconStyle}>
             <Link href="/rss.xml">
-              <IconRss height="h-4" />
+              <IconRss height={iconHeight} fill={iconFill} />
             </Link>
           </span>
         </div>
