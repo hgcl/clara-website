3162df49140af8495d4e9417089753bd9ca3b075
Clara <hey@clarale.com>
2021-01-05 14:03:10 +0100

    Change font-bold to font-medium

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import { WEBSITE_NAME } from "../lib/constants";
 export default function Header({ pageDescription }) {
   return (
     <>
-      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mt-8">
+      <h2 className="text-2xl md:text-4xl font-medium tracking-tight md:tracking-tighter leading-tight mt-8">
         <Link href="/">
           <a>{WEBSITE_NAME}</a>
         </Link>
