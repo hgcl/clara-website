3ebd7fe9cbc257a60b5ef49614df88b2da8d1b13
Clara <hey@clarale.com>
2021-01-07 19:11:32 +0100

    Add Microsub endpoint

--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,16 @@ export default function Index({ allPosts }) {
           crossorigin="anonymous"
         ></link>
         <title>Clara Le</title>
-        {/* Webmentions */}
+        {/* Following says where I want to receive webmentions */}
         <link
           rel="webmention"
           href="https://webmention.io/clarale.com/webmention"
         />
         <link rel="pingback" href="https://webmention.io/clarale.com/xmlrpc" />
-        {/* Authorization endpoint */}
+        {/* Authorization endpoint and set up microsub endpoint */}
         <link rel="authorization_endpoint" href="https://indieauth.com/auth" />
         <link rel="token_endpoint" href="https://tokens.indieauth.com/token" />
+        <link rel="microsub" href="https://aperture.p3k.io/microsub/591"/>
       </Head>
       <Container>
         <Intro />
