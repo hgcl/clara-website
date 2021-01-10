007c223d103090e54952c27c5a30172b49c65987
Clara <hey@clarale.com>
2021-01-10 22:12:55 +0100

    Set up a basic category filter on MorePosts

--- components/MorePosts.js
+++ components/MorePosts.js
@@ -5,20 +5,24 @@ import Container from "./container";
 import PostPreview from "./post-preview";
 
 export default function MorePosts({ posts }) {
+  const query = "review";
   return (
     <Container>
       <div className="max-w-2xl mx-auto">
-        {posts.map((post) => (
-          <PostPreview
-            key={post.slug}
-            title={post.title}
-            // coverImage={post.coverImage}
-            date={post.date}
-            // author={post.author}
-            slug={post.slug}
-            excerpt={post.excerpt}
-          />
-        ))}
+        {posts
+          .filter((post) => post.categories && post.categories.includes(query))
+          .map((post) => (
+            <PostPreview
+              key={post.slug}
+              title={post.title}
+              // coverImage={post.coverImage}
+              date={post.date}
+              // author={post.author}
+              slug={post.slug}
+              excerpt={post.excerpt}
+              categories={post.categories}
+            />
+          ))}
       </div>
     </Container>
   );
