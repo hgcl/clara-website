a27246b44c0d4dc0b5d954fdfa8a6bd1e8fd1392
Clara <hey@clarale.com>
2021-01-11 11:25:49 +0100

    Add Comment component to MDX files

--- components/AllPostComponents.js
+++ components/AllPostComponents.js
@@ -1,22 +1,24 @@
 import Column from "./Column";
+import Comment from "./Comment";
 import FeaturedArtistImage from "./FeaturedArtistImage";
 import Flex from "./Flex";
 import FigCaption from "./FigCaption";
 import Figure from "./Figure";
 import Link from "./link";
-import TopicTwoColumns from "./TopicTwoColumns";
 import ProgressBar from "./ProgressBar";
+import TopicTwoColumns from "./TopicTwoColumns";
 
 //import as many components you're using collectively in all your posts
 const AllPostComponents = {
   Column,
+  Comment,
   FeaturedArtistImage,
   Flex,
   FigCaption,
   Figure,
   Link,
-  TopicTwoColumns,
   ProgressBar,
+  TopicTwoColumns,
   // Any other component you want
 };
 export default AllPostComponents;
