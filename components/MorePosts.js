94b897863148e5106e7f5e069d71824af331dccc
Clara <hey@clarale.com>
2021-01-11 10:18:16 +0100

    Add category filter to /blog

--- components/MorePosts.js
+++ components/MorePosts.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 // Code very similar to more-stories.js
 
 import Header from "./header";
@@ -5,12 +7,35 @@ import Container from "./container";
 import PostPreview from "./post-preview";
 
 export default function MorePosts({ posts }) {
-  const query = "review";
+  const tagStyle = "pt-1/2 pb-1 px-3 mr-2 all-small-caps border-1";
+  const [filter, setFilter] = useState("");
   return (
     <Container>
       <div className="max-w-2xl mx-auto">
+        <div className="mb-16">
+          <button
+            className={`${
+              filter ? "border-gray-regular text-gray-regular" : "border-accent"
+            } ${tagStyle}`}
+            onClick={() => setFilter("")}
+          >
+            All Posts
+          </button>
+          <button
+            className={`${
+              filter === "review"
+                ? "border-accent"
+                : "border-gray-regular text-gray-regular"
+            } ${tagStyle}`}
+            onClick={() => setFilter("review")}
+          >
+            Reviews
+          </button>
+        </div>
         {posts
-          .filter((post) => post.categories && post.categories.includes(query))
+          .filter((post) =>
+            filter ? post.categories && post.categories.includes(filter) : post
+          )
           .map((post) => (
             <PostPreview
               key={post.slug}
