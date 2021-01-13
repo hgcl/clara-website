ac0fa4abfd214aee62ce3e186601202d48f396eb
Clara <hey@clarale.com>
2021-01-13 08:52:12 +0100

    Fix apostrophe

--- components/more-stories.js
+++ components/more-stories.js
@@ -4,7 +4,7 @@ export default function MoreStories({ posts }) {
   return (
     <section className="lg:flex">
       <h2 className="flex-auto mr-16 text-6xl md:text-7xl tracking-tighter leading-tight">
-        Here's What's New
+        Here’s What’s New
       </h2>
       <div className="flex-auto max-w-2xl mt-2">
         {posts.map((post) => (
