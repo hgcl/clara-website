d422086f7eb31d4bd3688768617ae1d0f5d559e3
Clara <hey@clarale.com>
2021-01-18 12:42:58 +0100

    :recycle: Refactor book modal with framer-motion

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,6 @@
 // import Alert from "../components/alert";
 import Footer from "./footer";
 import Meta from "./meta";
-// import Menu from "./Menu-v1";
 import Hcard from "./Hcard";
 import Menu from "./Menu";
 import { MenuToggle } from "./MenuToggle";
@@ -18,14 +17,14 @@ export default function Layout({ preview, children, title }) {
       <Meta title={title} />
       <div className="min-h-screen">
         {/* <Alert preview={preview} /> */}
-        <motion.nav className="z-50 sticky ml-auto right-0 top-8 md:top-12 w-16 md:w-24">
+        <motion.nav className="z-30 sticky ml-auto right-0 top-8 md:top-12 w-16 md:w-24">
+          <Menu open={open} onClose={closeModal} />
           {!open && (
             <MenuToggle
               toggle={() => setOpen((o) => !o)}
               className="absolute"
             />
           )}
-          <Menu open={open} onClose={closeModal} />
         </motion.nav>
         <Hcard />
         <main>{children}</main>
