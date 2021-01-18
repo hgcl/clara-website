69fdbdd15142a0d83bc2f7661dbfea82bb82b02e
Clara <hey@clarale.com>
2021-01-18 13:05:36 +0100

    :lipstick: Fix /Books view for 360px screens

--- components/BookGrid.js
+++ components/BookGrid.js
@@ -3,10 +3,10 @@ import BookNote from "../components/BookNote";
 export default function BookGrid({ books, dateLabel }) {
   // Unable to customize tailwindcss grid style with auto-fill. Adding it here instead:
   const gridStyle = {
-    "grid-template-columns": "repeat(auto-fill, minmax(9rem, 1fr))",
+    "grid-template-columns": "repeat(auto-fill, minmax(8rem, 1fr))",
   };
   return (
-    <section className="grid gap-y-8 gap-x-4 md:gap-x-6" style={gridStyle}>
+    <section className="grid gap-y-8 gap-x-4 md:gap-x-5" style={gridStyle}>
       {books
         .sort((a, b) => new Date(b.date) - new Date(a.date))
         .map((book) => {
