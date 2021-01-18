6c3fb934396de3258027980359ff04267f73ca69
Clara <hey@clarale.com>
2021-01-18 12:51:28 +0100

    :lipstick: Fix mobile view of footer

--- components/footer.js
+++ components/footer.js
@@ -14,8 +14,8 @@ export default function Footer() {
   const iconFill = "fill-current text-gray-regular";
   return (
     <footer className="bg-gray-dark h-56 mb-16">
-      <div className="flex flex-col items-center text-gray-regular h-full justify-end mb-16">
-        <div className="flex flex-row items-center mb-8">
+      <div className="flex flex-col items-center text-gray-regular h-full justify-end mb-16 mx-8">
+        <div className="flex flex-row items-center mb-8 mx-4">
           <span className={iconStyle}>
             <Link href="https://twitter.com/clara__le" aria-label="Twitter">
               <IconTwitter height={iconHeight} fill={iconFill} />
