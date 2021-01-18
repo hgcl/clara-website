137caf060d79f0263d86dfefb3f1857d6dc3f24b
Clara <hey@clarale.com>
2021-01-18 18:39:31 +0100

    :lipstick: Edit top padding of header

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import { WEBSITE_NAME } from "../lib/constants";
 export default function Header({ pageDescription }) {
   return (
     <>
-      <h2 className="text-2xl md:text-4xl tracking-tight md:tracking-tighter leading-tight pt-16 md:pt-4 mt-8">
+      <h2 className="text-2xl md:text-4xl tracking-tight md:tracking-tighter leading-tight pt-2 md:pt-4 mt-8">
         <Link href="/">
           <a>{WEBSITE_NAME}</a>
         </Link>
