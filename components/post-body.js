65b46a2d6eb354ccb0592772f30ae68868453843
Clara <hey@clarale.com>
2021-01-24 11:30:47 +0100

    :bug: Add {children} in PostBody again

--- components/post-body.js
+++ components/post-body.js
@@ -1,11 +1,12 @@
 import markdownStyles from "./markdown-styles.module.css";
 
-export default function PostBody({ content }) {
+export default function PostBody({ content, children }) {
   return (
     <div
       className={`e-content ${markdownStyles["markdown"]} prose prose-lg lg:prose-xl max-w-3xl mx-auto`}
     >
       {content}
+      {children}
     </div>
   );
 }
