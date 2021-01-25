434f65a9094ede42be15b77758deab01b89b1679
Clara <hey@clarale.com>
2021-01-25 16:31:13 +0100

    :construction: Ugly integration of movies into books system

---
+++ components/MovieGrid.js
@@ -0,0 +1,32 @@
+import BookNote from "./BookNote";
+
+export default function MovieGrid({ movies, dateLabel }) {
+  // Unable to customize tailwindcss grid style with auto-fill. Adding it here instead:
+  const gridStyle = {
+    "grid-template-columns": "repeat(auto-fill, minmax(8rem, 1fr))",
+  };
+  return (
+    <section className="grid gap-y-8 gap-x-4 md:gap-x-5" style={gridStyle}>
+      {movies
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+        .map((movie) => {
+          const { coverUrl, date, rating, review, title, tmdb_id } = movie;
+          return (
+            <article key={tmdb_id}>
+              <BookNote
+                coverUrl={coverUrl}
+                title={title}
+                author="{author}"
+                rating={rating}
+                notes="{notes}"
+                review={review}
+                dateLabel={dateLabel}
+                date={date}
+                // link={link}
+              />
+            </article>
+          );
+        })}
+    </section>
+  );
+}
--- pages/movies.js
+++ pages/movies.js
@@ -2,7 +2,7 @@ import { getAllMovies } from "../lib/getAllMovies";
 import Layout from "../components/layout";
 import Header from "../components/header";
 import Container from "../components/container";
-import Link from "../components/link";
+import MovieGrid from "../components/MovieGrid";
 import watchedData from "../data/watchedData.js";
 
 export default function Movies({ allMovies }) {
@@ -10,19 +10,21 @@ export default function Movies({ allMovies }) {
     <Layout title={"Movies"}>
       <Container>
         <Header pageDescription={"Get the popcorn ready"} />
-        <div>
+        <MovieGrid movies={allMovies} dateLabel="Watched on" />
+        {/* <div>
           {allMovies
             .sort((a, b) => new Date(b.date) - new Date(a.date))
             .map((allMovies) => {
-              const { tmdb_id, title, release_date, poster_path } = allMovies;
+              const { tmdb_id, title, release_date, coverUrl } = allMovies;
               return (
                 <article key={tmdb_id}>
                   {title}
-                  {poster_path}
+                  {coverUrl}
+                  <img src={coverUrl}></img>
                 </article>
               );
             })}
-        </div>
+        </div> */}
       </Container>
     </Layout>
   );
