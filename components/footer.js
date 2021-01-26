b6271d82fc3de208d1948719952ad9449e8b20e7
Clara <hey@clarale.com>
2021-01-26 16:47:36 +0100

    :lipstick: New footer layout

--- components/footer.js
+++ components/footer.js
@@ -1,70 +1,79 @@
-import Link from "./link";
-import { STAR, WAFFLE } from "../lib/constants";
-
-// icons
-import IconTwitter from "../public/icons/IconTwitter";
-import IconEtsy from "../public/icons/IconEtsy";
-import IconRss from "../public/icons/IconRss";
-import IconGithub from "../public/icons/IconGithub";
-
-// add fade in animation
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
+import NextLink from "next/link";
 
 export default function Footer() {
-  const iconStyle = "mx-2";
-  const svgStyle =
-    "h-4 fill-current text-gray-regular transition duration-500 hover:text-accent";
+  const styleList = "font-serif text-lg sm:text-2xl";
   return (
-    <footer className="z-0 h-64 sticky bottom-0">
-      <div className="bg-black flex flex-col items-center text-gray-regular h-full justify-end pb-24 px-8">
-        <div className="flex flex-row items-center mb-8 mx-4">
-          <span className={iconStyle}>
-            <Link href="https://twitter.com/clara__le" aria-label="Twitter">
-              <IconTwitter svgClassName={svgStyle} />
-            </Link>
-          </span>
-          <span className={iconStyle}>
-            <Link href="https://lifeundercontrol.etsy.com" aria-label="Etsy">
-              <IconEtsy svgClassName={svgStyle} />
-            </Link>
-          </span>
-          <span className={iconStyle}>
-            <Link href="https://github.com/hgcl/" aria-label="GitHub">
-              <IconGithub svgClassName={svgStyle} />
-            </Link>
-          </span>
-          <span className={iconStyle}>
-            <Link href="/rss.xml">
-              <IconRss svgClassName={svgStyle} aria-label="RSS" />
-            </Link>
-          </span>
+    <footer className="z-0 h-72 sticky right-0 bottom-0 bg-black text-gray-regular text-right">
+      <div className="flex flex-row flex-wrap max-w-5xl my-auto ml-auto px-8 pt-8">
+        <div className="flex-1 pb-8">
+          <h2 className="comment pb-4">Social</h2>
+          <ul className={`${styleList} list-none`}>
+            <li>
+              <NextLink href="https://twitter.com/clara__le">
+                <a>Twitter</a>
+              </NextLink>
+            </li>
+            <li>
+              <NextLink href="https://lifeundercontrol.etsy.com">
+                <a>Etsy</a>
+              </NextLink>
+            </li>
+            <li>
+              <NextLink href="https://github.com/hgcl/">
+                <a>GitHub</a>
+              </NextLink>
+            </li>
+            <li>
+              <NextLink href="/rss.xml">
+                <a>RSS</a>
+              </NextLink>
+            </li>
+          </ul>
         </div>
-        <p className="text-center">
-          Notice something wrong?{" "}
-          <Link href="https://github.com/hgcl/clara-website/issues">
-            File an issue!
-          </Link>{" "}
-          —{" "}
-          <Link
-            className="text-xl"
-            href="/favorites"
-            aria-label="favorites page"
-          >
-            {STAR}
-          </Link>{" "}
-          <Link
-            className="text-xl"
-            href="/recipe-book"
-            aria-label="Recipe page"
-          >
-            {WAFFLE}
-          </Link>
-        </p>
+        <div className="flex-2 pl-16 sm:pl-24 max-w-screen-sm">
+          <h2 className="comment pb-4">Sitemap</h2>
+          <p className={`${styleList}`}>
+            <NextLink href="/about">
+              <a>About</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/about-this-website">
+              <a>About&nbsp;this&nbsp;website</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/antilibrary">
+              <a>Antilibrary</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/blog">
+              <a>Blog</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/books">
+              <a>Books</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/favorites">
+              <a>Favorites</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/movies">
+              <a>Movies</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/now">
+              <a>Now</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/portfolio">
+              <a>Portfolio</a>
+            </NextLink>
+            &nbsp;—{" "}
+            <NextLink href="/recipe-book">
+              <a>Recipe&nbsp;Book</a>
+            </NextLink>
+          </p>
+        </div>
       </div>
     </footer>
   );
