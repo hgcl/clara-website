23b7816b7d3b2c87d3ee24e9e22ab11acedd5f0e
Clara <hey@clarale.com>
2021-01-27 08:41:15 +0100

    :lipstick: Change spacing between pages in Footer

--- components/footer.js
+++ components/footer.js
@@ -2,8 +2,8 @@ import NextLink from "next/link";
 
 export default function Footer() {
   const ulStyle = "font-serif text-lg sm:text-2xl";
-  const liStyle = "sm:inline-block";
-  const pageSeparator = <span className="hidden sm:inline">&nbsp;— </span>;
+  const liStyle = "pl-2 sm:inline-block";
+  const pageSeparator = <span className="hidden sm:inline">&nbsp;—</span>;
   return (
     <footer className="z-0 h-96 sm:h-72 sticky right-0 bottom-0 bg-black text-gray-regular text-right">
       <div className="flex flex-row flex-wrap max-w-5xl my-auto ml-auto px-8 pt-8">
@@ -39,51 +39,51 @@ export default function Footer() {
               <NextLink href="/about">
                 <a>About</a>
               </NextLink>
+              {pageSeparator}
             </li>
             <li className={liStyle}>
-              {pageSeparator}
               <NextLink href="/about-this-website">
                 <a>About&nbsp;this&nbsp;website</a>
               </NextLink>
+              {pageSeparator}
             </li>
             <li className={liStyle}>
-              {pageSeparator}
               <NextLink href="/antilibrary">
                 <a>Antilibrary</a>
               </NextLink>
+              {pageSeparator}
             </li>
-            {pageSeparator}
             <li className={liStyle}>
               <NextLink href="/blog">
                 <a>Blog</a>
               </NextLink>
+              {pageSeparator}
             </li>
             <li className={liStyle}>
-              {pageSeparator}
               <NextLink href="/books">
                 <a>Books</a>
               </NextLink>
+              {pageSeparator}
             </li>
             <li className={liStyle}>
-              {pageSeparator}
               <NextLink href="/favorites">
                 <a>Favorites</a>
               </NextLink>
+              {pageSeparator}
             </li>
             <li className={liStyle}>
-              {pageSeparator}
               <NextLink href="/movies">
                 <a>Movies</a>
               </NextLink>
+              {pageSeparator}
             </li>
             <li className={liStyle}>
-              {pageSeparator}
               <NextLink href="/now">
                 <a>Now</a>
               </NextLink>
+              {pageSeparator}
             </li>
             <li className={liStyle}>
-              {pageSeparator}
               <NextLink href="/portfolio">
                 <a>Portfolio</a>
               </NextLink>
