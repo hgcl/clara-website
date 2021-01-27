cf915ea929e4f65bd24bb48fed6b0f9d03d22acf
Clara <hey@clarale.com>
2021-01-27 07:58:58 +0100

    :lipstick: Customize mobile view of Footer

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,15 @@
 import NextLink from "next/link";
 
 export default function Footer() {
-  const styleList = "font-serif text-lg sm:text-2xl";
+  const ulStyle = "font-serif text-lg sm:text-2xl";
+  const liStyle = "sm:inline-block";
+  const pageSeparator = <span className="hidden sm:inline">&nbsp;— </span>;
   return (
-    <footer className="z-0 h-72 sticky right-0 bottom-0 bg-black text-gray-regular text-right">
+    <footer className="z-0 h-96 sm:h-72 sticky right-0 bottom-0 bg-black text-gray-regular text-right">
       <div className="flex flex-row flex-wrap max-w-5xl my-auto ml-auto px-8 pt-8">
         <div className="flex-1 pb-8">
           <h2 className="comment pb-4">Social</h2>
-          <ul className={`${styleList} list-none`}>
+          <ul className={`${ulStyle} list-none`}>
             <li>
               <NextLink href="https://twitter.com/clara__le">
                 <a>Twitter</a>
@@ -32,47 +34,67 @@ export default function Footer() {
         </div>
         <div className="flex-2 pl-16 sm:pl-24 max-w-screen-sm">
           <h2 className="comment pb-4">Sitemap</h2>
-          <p className={`${styleList}`}>
-            <NextLink href="/about">
-              <a>About</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/about-this-website">
-              <a>About&nbsp;this&nbsp;website</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/antilibrary">
-              <a>Antilibrary</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/blog">
-              <a>Blog</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/books">
-              <a>Books</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/favorites">
-              <a>Favorites</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/movies">
-              <a>Movies</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/now">
-              <a>Now</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/portfolio">
-              <a>Portfolio</a>
-            </NextLink>
-            &nbsp;—{" "}
-            <NextLink href="/recipe-book">
-              <a>Recipe&nbsp;Book</a>
-            </NextLink>
-          </p>
+          <ul className={`${ulStyle} list-none`}>
+            <li className={liStyle}>
+              <NextLink href="/about">
+                <a>About</a>
+              </NextLink>
+            </li>
+            <li className={liStyle}>
+              {pageSeparator}
+              <NextLink href="/about-this-website">
+                <a>About&nbsp;this&nbsp;website</a>
+              </NextLink>
+            </li>
+            <li className={liStyle}>
+              {pageSeparator}
+              <NextLink href="/antilibrary">
+                <a>Antilibrary</a>
+              </NextLink>
+            </li>
+            {pageSeparator}
+            <li className={liStyle}>
+              <NextLink href="/blog">
+                <a>Blog</a>
+              </NextLink>
+            </li>
+            <li className={liStyle}>
+              {pageSeparator}
+              <NextLink href="/books">
+                <a>Books</a>
+              </NextLink>
+            </li>
+            <li className={liStyle}>
+              {pageSeparator}
+              <NextLink href="/favorites">
+                <a>Favorites</a>
+              </NextLink>
+            </li>
+            <li className={liStyle}>
+              {pageSeparator}
+              <NextLink href="/movies">
+                <a>Movies</a>
+              </NextLink>
+            </li>
+            <li className={liStyle}>
+              {pageSeparator}
+              <NextLink href="/now">
+                <a>Now</a>
+              </NextLink>
+            </li>
+            <li className={liStyle}>
+              {pageSeparator}
+              <NextLink href="/portfolio">
+                <a>Portfolio</a>
+              </NextLink>
+              {pageSeparator}
+            </li>
+            <li className={liStyle}>
+              <NextLink href="/recipe-book">
+                <a>Recipe&nbsp;Book</a>
+              </NextLink>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
