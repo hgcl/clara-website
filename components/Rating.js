a00229a98fed94bee6822b85b2d98cc78f9a308e
Clara <hey@clarale.com>
2021-01-28 10:14:40 +0100

    :lipstick: Change rating system into stars

--- components/Rating.js
+++ components/Rating.js
@@ -1,18 +1,39 @@
+import { STAR, ARROW } from "../lib/constants";
+import RatingIcon from "../public/icons/RatingIcon";
+
 const Rating = ({ rating, pRating }) => {
-  const dotStyle = "rounded w-2 h-2 border-1 border-gray-regular mr-1";
-  const emptyDots = 5 - rating;
+  // Dot Rating:
+  //   const dotStyle = "rounded w-2 h-2 border-1 border-gray-regular mr-1";
+  //   const emptyDots = 5 - rating;
+  //   return (
+  //     <div className={`${pRating && "p-rating"} flex mt-2`} value={rating}>
+  //       {[...Array(rating)].map((_, index) => (
+  //         <span
+  //           key={`dot-full-${index}`}
+  //           className={`${dotStyle} bg-gray-regular`}
+  //         />
+  //       ))}
+  //       {[...Array(emptyDots)].map((_, index) => (
+  //         <span key={`dot-empty-${index}`} className={`${dotStyle}`} />
+  //       ))}
+  //     </div>
+  //   );
+  // };
+
+  // Star Rating:
   return (
-    <div className={`${pRating && "p-rating"} flex mt-2`} value={rating}>
+    <span
+      className={`${pRating && "p-rating"} flex justify-center`}
+      value={rating}
+    >
       {[...Array(rating)].map((_, index) => (
-        <span
+        <RatingIcon
           key={`dot-full-${index}`}
-          className={`${dotStyle} bg-gray-regular`}
+          className="text-white"
+          height="15"
         />
       ))}
-      {[...Array(emptyDots)].map((_, index) => (
-        <span key={`dot-empty-${index}`} className={`${dotStyle}`} />
-      ))}
-    </div>
+    </span>
   );
 };
 
