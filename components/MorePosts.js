731f9753e8a838f134f61f0c43871faf491c21c2
Clara <hey@clarale.com>
2021-01-28 16:45:21 +0100

    :recycle: Add tag styles in constant

--- components/MorePosts.js
+++ components/MorePosts.js
@@ -7,30 +7,28 @@ import PostPreview from "./post-preview";
 
 export default function MorePosts({ posts }) {
   const tagStyle = "pt-1 pb-2 px-4 mr-4 all-small-caps border-1 rounded-sm";
+  const inactiveTag = "border-gray-regular text-gray-regular";
+  const activeTag = "border-accent";
   const [filter, setFilter] = useState("");
   return (
     <Container>
       <div className="max-w-2xl mx-auto">
-        <div className="mb-16">
+        <section className="mb-16">
           <button
-            className={`${
-              filter ? "border-gray-regular text-gray-regular" : "border-accent"
-            } ${tagStyle}`}
+            className={`${filter ? inactiveTag : activeTag} ${tagStyle}`}
             onClick={() => setFilter("")}
           >
             All Posts
           </button>
           <button
             className={`${
-              filter === "weekly-review"
-                ? "border-accent"
-                : "border-gray-regular text-gray-regular"
+              filter === "weekly-review" ? activeTag : inactiveTag
             } ${tagStyle}`}
             onClick={() => setFilter("weekly-review")}
           >
             Reviews
           </button>
-        </div>
+        </section>
         {posts
           .filter((post) =>
             filter ? post.categories && post.categories.includes(filter) : post
