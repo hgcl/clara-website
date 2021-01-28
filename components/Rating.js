5047f5a691306783b1fbd7fc5a34073ff26584df
Clara <hey@clarale.com>
2021-01-28 10:31:08 +0100

    :lipstick: Add empty rating stars

--- components/Rating.js
+++ components/Rating.js
@@ -1,35 +1,21 @@
 import { STAR, ARROW } from "../lib/constants";
-import RatingIcon from "../public/icons/RatingIcon";
+import { RatingIconFull, RatingIconEmpty } from "../public/icons/RatingIcon";
 
 const Rating = ({ rating, pRating }) => {
-  // Dot Rating:
-  //   const dotStyle = "rounded w-2 h-2 border-1 border-gray-regular mr-1";
-  //   const emptyDots = 5 - rating;
-  //   return (
-  //     <div className={`${pRating && "p-rating"} flex mt-2`} value={rating}>
-  //       {[...Array(rating)].map((_, index) => (
-  //         <span
-  //           key={`dot-full-${index}`}
-  //           className={`${dotStyle} bg-gray-regular`}
-  //         />
-  //       ))}
-  //       {[...Array(emptyDots)].map((_, index) => (
-  //         <span key={`dot-empty-${index}`} className={`${dotStyle}`} />
-  //       ))}
-  //     </div>
-  //   );
-  // };
-
-  // Star Rating:
+  const emptyRating = 5 - rating;
   return (
-    <span
-      className={`${pRating && "p-rating"} flex justify-center`}
-      value={rating}
-    >
+    <span className={`${pRating && "p-rating"} flex flex-row`} value={rating}>
       {[...Array(rating)].map((_, index) => (
-        <RatingIcon
-          key={`dot-full-${index}`}
+        <RatingIconFull
+          key={`rating-full-${index}`}
           className="text-white"
+          height="15"
+        />
+      ))}
+      {[...Array(emptyRating)].map((_, index) => (
+        <RatingIconEmpty
+          key={`rating-empty-${index}`}
+          className="text-gray-regular"
           height="15"
         />
       ))}
