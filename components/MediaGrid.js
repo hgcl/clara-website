ad79fbbf0bc1fa765fb2e37f7bd336c49d42ee80
Clara <hey@clarale.com>
2021-01-28 16:45:07 +0100

    :sparkles: Add first filters for Media

--- components/MediaGrid.js
+++ components/MediaGrid.js
@@ -1,78 +1,120 @@
+import { useState } from "react";
 import MediaTile from "./MediaTile";
+import { getYear } from "date-fns";
+
+// Dropdown:
 
 export default function MediaGrid({ items, dateLabel, type }) {
   // Unable to customize tailwindcss grid style with auto-fill. Adding it here instead:
   const gridStyle = {
     "grid-template-columns": "repeat(auto-fill, minmax(8rem, 1fr))",
   };
-  return (
-    <section className="grid gap-x-1 xs:gap-x-4 gap-y-8" style={gridStyle}>
-      {type === "isBook" &&
-        items
-          .sort((a, b) => new Date(b.date) - new Date(a.date))
-          .map((book) => {
-            const {
-              author,
-              coverUrl,
-              date,
-              isbn,
-              link,
-              notes,
-              rating,
-              review,
-              title,
-            } = book;
+  // Mediafilters
+  const [yearFilter, setYear] = useState(0);
+  const tagStyle = "pt-1 pb-2 px-4 mr-4 all-small-caps border-1 rounded-sm";
+  const inactiveTag = "border-gray-regular text-gray-regular";
+  const activeTag = "border-accent";
 
-            return (
-              <article key={isbn}>
-                <MediaTile
-                  author={author}
-                  coverUrl={coverUrl}
-                  date={date}
-                  dateLabel={dateLabel}
-                  link={link}
-                  notes={notes}
-                  rating={rating}
-                  review={review}
-                  title={title}
-                />
-              </article>
-            );
-          })}
-      {type === "isMovie" &&
-        items
-          .sort(
-            (a, b) =>
-              new Date(b.date || b.release_date) -
-              new Date(a.date || a.release_date)
-          )
-          .map((movie) => {
-            const {
-              coverUrl,
-              date,
-              notes,
-              rating,
-              release_date,
-              review,
-              title,
-              tmdb_id,
-            } = movie;
-            return (
-              <article key={tmdb_id}>
-                <MediaTile
-                  // link={link}
-                  coverUrl={coverUrl}
-                  date={date || release_date}
-                  dateLabel={dateLabel}
-                  notes={notes}
-                  rating={rating}
-                  review={review}
-                  title={title}
-                  type={type}
-                />
-              </article>
-            );
-          })}
-    </section>
+  return (
+    <>
+      <section className="mb-16">
+        <button
+          className={`${yearFilter ? inactiveTag : activeTag} ${tagStyle}`}
+          onClick={() => setYear(0)}
+        >
+          All Books
+        </button>
+        <button
+          className={`${
+            yearFilter === 2021 ? activeTag : inactiveTag
+          } ${tagStyle}`}
+          onClick={() => setYear(2021)}
+        >
+          2021
+        </button>
+        <button
+          className={`${
+            yearFilter === 2020 ? activeTag : inactiveTag
+          } ${tagStyle}`}
+          onClick={() => setYear(2020)}
+        >
+          2020
+        </button>
+      </section>
+      <section className="grid gap-x-1 xs:gap-x-4 gap-y-8" style={gridStyle}>
+        {type === "isBook" &&
+          items
+            .filter((book) =>
+              yearFilter ? getYear(new Date(book.date)) === yearFilter : book
+            )
+            .sort((a, b) => new Date(b.date) - new Date(a.date))
+            .map((book) => {
+              const {
+                author,
+                coverUrl,
+                date,
+                isbn,
+                link,
+                notes,
+                rating,
+                review,
+                title,
+              } = book;
+
+              return (
+                <article key={isbn}>
+                  <MediaTile
+                    author={author}
+                    coverUrl={coverUrl}
+                    date={date}
+                    dateLabel={dateLabel}
+                    link={link}
+                    notes={notes}
+                    rating={rating}
+                    review={review}
+                    title={title}
+                  />
+                </article>
+              );
+            })}
+        {type === "isMovie" &&
+          items
+            .filter((movie) =>
+              yearFilter ? getYear(new Date(movie.date)) === yearFilter : movie
+            )
+            .sort(
+              (a, b) =>
+                new Date(b.date || b.release_date) -
+                new Date(a.date || a.release_date)
+            )
+            .map((movie) => {
+              const {
+                coverUrl,
+                date,
+                notes,
+                rating,
+                release_date,
+                review,
+                title,
+                tmdb_id,
+              } = movie;
+              return (
+                <article key={tmdb_id}>
+                  <MediaTile
+                    // link={link}
+                    coverUrl={coverUrl}
+                    date={date || release_date}
+                    dateLabel={dateLabel}
+                    notes={notes}
+                    rating={rating}
+                    review={review}
+                    title={title}
+                    type={type}
+                  />
+                </article>
+              );
+            })}
+      </section>
+    </>
   );
 }
--- components/MediaTile.js
+++ components/MediaTile.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { CLOSE, ARROW } from "../lib/constants";
-import Link from "./link";
+import { motion } from "framer-motion";
 import DateFormatter from "./date-formatter";
 import Rating from "./Rating";
 import Image from "next/image";
 import NoteIcon from "../public/icons/NoteIcon";
+import OpenMediaModal from "./OpenMediaModal";
 
 export default function MediaTile({
   coverUrl,
@@ -52,7 +51,7 @@ export default function MediaTile({
           />
         </div>
       )}
-      <OpenModal
+      <OpenMediaModal
         open={open}
         onClose={closeModal}
         title={title}
@@ -109,80 +108,5 @@ const ClosedTile = ({
         </div>
       </div>
     </>
-  );
-};
-
-const OpenModal = ({
-  open,
-  onClose,
-  title,
-  author,
-  notes,
-  review,
-  rating,
-  date,
-  dateLabel,
-  link,
-}) => {
-  const keydownHandler = ({ key }) => {
-    switch (key) {
-      case "Escape":
-        onClose();
-        break;
-      default:
-    }
-  };
-
-  React.useEffect(() => {
-    document.addEventListener("keydown", keydownHandler);
-    return () => document.removeEventListener("keydown", keydownHandler);
-  });
-  return (
-    <AnimatePresence>
-      {open && (
-        <motion.div
-          key="modal"
-          exit={{ opacity: 0 }}
-          className="h-review z-40 absolute top-0 left-0 w-screen h-screen"
-          onClick={onClose}
-        >
-          <div
-            className="fixed top-0 left-0 h-screen w-screen md:max-w-2xl m-auto bg-black bg-opacity-90"
-            onClick={onClose}
-          >
-            <div className="sticky ml-auto w-12">
-              <button aria-label="Close" className="absolute text-4xl top-3">
-                {CLOSE}
-              </button>
-            </div>
-            <div onClick={onClose} className="flex items-center h-screen p-8">
-              <div className="text-xl relative w-full">
-                <div className="text-center md:text-left mb-6">
-                  <p className="comment tracking mb-4 md:fixed md:top-6">
-                    {review ? "Review" : "Notes"}
-                  </p>
-                  <h2 className="p-name text-3xl leading-snug mb-1">{title}</h2>
-                  {author && <p>by {author}</p>}
-                </div>
-                <p className="e-content mb-8">{review ? review : notes}</p>
-                {rating && (
-                  <p className="text-gray-regular mb-4">
-                    <Rating rating={rating} pRating height="16" />
-                  </p>
-                )}
-                {link && (
-                  <div className="mb-2 comment">
-                    <Link href={link}>Recommendation source&ensp;{ARROW}</Link>
-                  </div>
-                )}
-                <p className="text-base text-gray-regular">
-                  {dateLabel} <DateFormatter dateString={date} dtPublished />
-                </p>
-              </div>
-            </div>
-          </div>
-        </motion.div>
-      )}
-    </AnimatePresence>
   );
 };
