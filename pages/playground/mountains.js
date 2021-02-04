abcb771d764ced1463f5b30d86c3a83ef897e10b
Clara <hey@clarale.com>
2021-02-04 18:23:31 +0100

    :pen: New draft of Mountains page

---
+++ pages/playground/mountains.js
@@ -0,0 +1,13 @@
+import Layout from "../../components/layout";
+import Header from "../../components/header";
+import Container from "../../components/container";
+
+export default function Mountains({}) {
+  return (
+    <Layout title="Mountains" showAlert>
+      <Container>
+        <Header pageDescription={"Mountains"} />
+      </Container>
+    </Layout>
+  );
+}
