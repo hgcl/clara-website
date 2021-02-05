5134370df31c9a533ba7236168334d49bf521cf8
Clara <hey@clarale.com>
2021-02-05 14:02:30 +0100

    :lipstick: Add settings button to recipe-book page

--- components/MoreRecipes.js
+++ components/MoreRecipes.js
@@ -4,6 +4,7 @@ import Container from "./container";
 import RecipePostPreview from "./RecipePostPreview";
 
 import DropdownButton from "../components/DropdownButton";
+import SettingsIcon from "../public/icons/SettingsIcon";
 
 export default function MoreRecipes({ posts }) {
   // Unable to customize tailwindcss grid style with auto-fill. Adding it here instead:
@@ -11,6 +12,7 @@ export default function MoreRecipes({ posts }) {
     gridTemplateColumns: "repeat(auto-fill, minmax(14rem, 1fr))",
   };
   // filters
+  const [openSettings, setSettings] = useState(false);
   const [dietFilter, setDiet] = useState(0);
   const dietDropdown = [
     { name: "All", value: 0 },
@@ -39,43 +41,68 @@ export default function MoreRecipes({ posts }) {
     { name: "Medium", value: 2 },
     { name: "Hard", value: 3 },
   ];
+
+  const dropdownList = (className) => (
+    <>
+      <DropdownButton
+        className={className}
+        title="Diet"
+        filter={dietFilter}
+        setHook={setDiet}
+        itemsArray={dietDropdown}
+        widthButton="w-24"
+        widthModal="w-28"
+      />
+      <DropdownButton
+        className={className}
+        title="Season"
+        filter={seasonFilter}
+        setHook={setSeason}
+        itemsArray={seasonDropdown}
+      />
+      <DropdownButton
+        className={className}
+        title="Difficulty"
+        filter={difficultyFilter}
+        setHook={setDifficulty}
+        itemsArray={difficultyDropdown}
+        widthButton="w-24"
+        widthModal="w-28"
+      />
+      <DropdownButton
+        className={className}
+        title="Duration"
+        filter={durationFilter}
+        setHook={setDuration}
+        itemsArray={durationDropdown}
+        widthButton="w-24"
+        widthModal="w-28"
+      />
+    </>
+  );
   return (
     <Container>
-      <div className="flex flex-col sm:flex-row flex-wrap">
-        <DropdownButton
-          title="Diet"
-          filter={dietFilter}
-          setHook={setDiet}
-          itemsArray={dietDropdown}
-          marginRight="mr-2"
-          widthButton="w-24"
-          widthModal="w-28"
-        />
-        <DropdownButton
-          title="Season"
-          filter={seasonFilter}
-          setHook={setSeason}
-          itemsArray={seasonDropdown}
-          marginRight="mr-2"
-        />
-        <DropdownButton
-          title="Difficulty"
-          filter={difficultyFilter}
-          setHook={setDifficulty}
-          itemsArray={difficultyDropdown}
-          marginRight="mr-2"
-          widthButton="w-24"
-          widthModal="w-28"
-        />
-        <DropdownButton
-          title="Duration"
-          filter={durationFilter}
-          setHook={setDuration}
-          itemsArray={durationDropdown}
-          marginRight="mr-4"
-          widthButton="w-24"
-          widthModal="w-28"
-        />
+      <div className="flex flex-row flex-wrap">
+        {dropdownList("hidden xs:block")}
+        <div className="flex flex-row justify-start w-full xs:w-auto">
+          <button
+            onClick={() => setSettings(!openSettings)}
+            className="xs:hidden"
+          >
+            <SettingsIcon />
+          </button>
+          <div className="xs:hidden w-full" />
+        </div>
+        {/* Open generic settings dropdown: */}
+        {openSettings && (
+          <div className="xs:hidden my-4">
+            <div
+              className="z-10 absolute -top-8 left-0 h-full w-full"
+              onClick={() => setSettings(!openSettings)}
+            />
+            <div className="z-10 relative w-full">{dropdownList("")}</div>
+          </div>
+        )}
       </div>
       <div className="grid gap-y-6 gap-x-8 mt-6" style={gridStyle}>
         {posts
