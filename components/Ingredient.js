aa1f9050986e7bd4542efd66f8fe7d09299195c5
Clara <hey@clarale.com>
2021-02-06 22:27:55 +0100

    :pen: Update recipes

--- components/Ingredient.js
+++ components/Ingredient.js
@@ -1,18 +1,22 @@
 // hRecipe microformats (ingredient list)
 
-export default function Ingredient({ v, t, i, notList }) {
+import Link from "./link";
+
+export default function Ingredient({ v, t, i, link, notList, optional }) {
+  const details = (
+    <>
+      <span className="value">{v && v}</span>{" "}
+      <span className="type">{t && t}</span>{" "}
+      {link ? <Link href={link}>{i}</Link> : i}
+      <span className="comment">&emsp;{optional && "optional"}</span>
+    </>
+  );
   return (
     <>
       {notList ? (
-        <span className="ingredient">
-          <span className="value">{v && v}</span>{" "}
-          <span className="type">{t && t}</span> {i}
-        </span>
+        <span className="ingredient">{details}</span>
       ) : (
-        <li className="ingredient">
-          <span className="value">{v && v}</span>{" "}
-          <span className="type">{t && t}</span> {i}
-        </li>
+        <li className="ingredient">{details}</li>
       )}
     </>
   );
