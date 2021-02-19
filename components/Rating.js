f20118c419f7992c7746b49c4e583e94b06c7682
Clara <hey@clarale.com>
2021-02-19 08:35:36 +0100

    :recycle: Update with fixed icon dimensions 24x24

--- components/Rating.js
+++ components/Rating.js
@@ -7,15 +7,15 @@ const Rating = ({ rating, pRating, width, color }) => {
       {[...Array(rating)].map((_, index) => (
         <RatingIconFull
           key={`rating-full-${index}`}
-          className={`${color || "text-white"} mr-2px`}
-          width={width || 12}
+          className={`${color || "text-white"}`}
+          width={width || 14}
         />
       ))}
       {[...Array(emptyRating)].map((_, index) => (
         <RatingIconEmpty
           key={`rating-empty-${index}`}
-          className="text-gray-regular mr-2px"
-          width={width || 14}
+          className="text-gray-regular"
+          width={width || 16}
         />
       ))}
     </span>
