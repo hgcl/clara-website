04f616ca2e387a495a4006eee33c94aa12d8a991
Clara <hey@clarale.com>
2021-04-03 08:39:20 +0200

    :lipstick: Hide latest posts on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Container from "../components/container";
-import MoreStories from "../components/more-stories";
+// import MoreStories from "../components/more-stories";
 // import HeroPost from '../components/hero-post'
 import Intro from "../components/intro";
 import Layout from "../components/layout";
-import { getAllPosts } from "../lib/getAllPosts";
+// import { getAllPosts } from "../lib/getAllPosts";
 
 export default function Index({ allPosts }) {
   // const heroPost = allPosts[0]
-  const latestPosts = allPosts.slice(0, 3);
+  // const latestPosts = allPosts.slice(0, 3);
   return (
     <Layout noHentry>
       <Container>
@@ -22,23 +22,23 @@ export default function Index({ allPosts }) {
               excerpt={heroPost.excerpt}
             />
           )} */}
-        {allPosts.length > 0 && <MoreStories posts={latestPosts} />}
+        {/* {allPosts.length > 0 && <MoreStories posts={latestPosts} />} */}
       </Container>
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    // 'author',
-    // 'coverImage',
-    "excerpt",
-  ]);
+// export async function getStaticProps() {
+//   const allPosts = getAllPosts([
+//     "title",
+//     "date",
+//     "slug",
+//     // 'author',
+//     // 'coverImage',
+//     "excerpt",
+//   ]);
 
-  return {
-    props: { allPosts },
-  };
-}
+//   return {
+//     props: { allPosts },
+//   };
+// }
