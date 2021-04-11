35196f470a71774ebc4d25bf6df3806c5eabda5e
Clara <hey@clarale.com>
2021-04-11 09:05:47 +0200

    :bug: Fix rating styling on Safari

--- components/Rating.js
+++ components/Rating.js
@@ -9,6 +9,7 @@ const Rating = ({ rating, pRating, width, color }) => {
           key={`rating-full-${index}`}
           className={`${color || "text-white"}`}
           width={width || 14}
+          height={width || 14}
         />
       ))}
       {[...Array(emptyRating)].map((_, index) => (
@@ -16,6 +17,7 @@ const Rating = ({ rating, pRating, width, color }) => {
           key={`rating-empty-${index}`}
           className="text-gray-regular"
           width={width || 16}
+          height={width || 16}
         />
       ))}
     </span>
