b2d958c6228365c292967003465e610026624efe
Clara <hey@clarale.com>
2021-04-11 09:32:38 +0200

    :bug: Fix dropdown style for Safari

--- components/OpenDropdown.js
+++ components/OpenDropdown.js
@@ -18,7 +18,7 @@ export default function OpenDropdown({
         <button
           role="option"
           key={item.name}
-          className={`${dropdownRow} ${commonStyle} pt-2 hover:text-accent focus:text-accent active:text-accent text-gray-dark`}
+          className={`${dropdownRow} ${commonStyle} hover:text-accent focus:text-accent active:text-accent text-gray-dark`}
           onClick={() => {
             setHook(item.value);
             setOpen(!open);
@@ -60,11 +60,11 @@ export default function OpenDropdown({
             <div
               className={`${
                 widthModal || "w-24"
-              } ${commonStyle} absolute flex flex-col top-1 left-0 rounded-t h-full -mt-10 pt-2`}
+              } ${commonStyle} absolute flex flex-col top-0 left-0 rounded-t rounded-b min-h-full -mt-12 pt-2 pb-2`}
             >
               <button
                 role="button"
-                className={`${dropdownRow} ${commonStyle} pb-2 all-small-caps flex flex-row items-center`}
+                className={`${dropdownRow} ${commonStyle} all-small-caps flex flex-row items-center`}
                 onClick={() => setOpen(!open)}
               >
                 <span className="pr-2">{title}</span>
@@ -73,9 +73,6 @@ export default function OpenDropdown({
                 </span>
               </button>
               {itemsMapped()}
-              <div
-                className={`${commonStyle} w-full h-full rounded-b pb-2`}
-              ></div>
             </div>
           </motion.div>
         </>
