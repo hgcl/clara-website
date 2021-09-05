178d62d72752ac1442b7b3e455b46d41e965c1fb
hgcl <hey@clarale.com>
2021-09-05 14:47:29 +0200

    :bug: :lipstick: Fix menu Zindex and layout change

--- components/Menu.js
+++ components/Menu.js
@@ -1,4 +1,4 @@
-import Link from "./link";
+import NextLink from "next/link";
 import { CLOSE } from "../lib/constants";
 import React from "react";
 import PropTypes from "prop-types";
@@ -10,6 +10,11 @@ import { motion, AnimatePresence } from "framer-motion";
  * Users must interact with the modal window before they can return to the parent application.
  */
 const Menu = ({ open, hideCloseButton, setOpen }) => {
+  const ulStyle =
+    "flex-1 font-serif text-lg sm:text-2xl max-w-3xl mx-auto px-8";
+  const liStyle = "pl-2";
+  const linkStyle = "hover:text-gray-darkest focus:text-gray-darkest";
+
   const keydownHandler = ({ key }) => {
     switch (key) {
       case "Escape":
@@ -30,12 +35,12 @@ const Menu = ({ open, hideCloseButton, setOpen }) => {
         <motion.div key="modal" exit={{ opacity: 0 }}>
           {/* Modal container / overlay */}
           <div
-            className="fixed w-screen h-screen"
+            className="z-50 fixed w-screen h-screen"
             onClick={() => setOpen(false)}
           >
             {/* Modal pop-up */}
             <div
-              className="fixed top-0 left-0 flex flex-col h-screen w-screen m-auto bg-accent-dark text-gray-lightest text-center justify-center items-center"
+              className="fixed top-0 left-0 flex flex-col h-screen w-screen m-auto bg-accent-dark text-gray-lightest  justify-center items-center"
               onClick={() => setOpen(false)}
             >
               {!hideCloseButton && (
@@ -43,26 +48,79 @@ const Menu = ({ open, hideCloseButton, setOpen }) => {
                   <button onClick={() => setOpen(false)}>{CLOSE}</button>
                 </div>
               )}
-              <ul className="relative text-3xl list-none">
-                <li onClick={() => setOpen(false)}>
-                  <Link href="/">Home</Link>
-                </li>
-                <li onClick={() => setOpen(false)}>
-                  <Link href="/about">About</Link>
-                </li>
-                <li onClick={() => setOpen(false)}>
-                  <Link href="/now">Now</Link>
-                </li>
-                <li onClick={() => setOpen(false)}>
-                  <Link href="/blog">Writing</Link>
-                </li>
-                <li onClick={() => setOpen(false)}>
-                  <Link href="/books">Reading</Link>
-                </li>
-                <li onClick={() => setOpen(false)}>
-                  <Link href="/portfolio">Portfolio</Link>
-                </li>
-              </ul>
+              <nav className="sm:flex text-center sm:text-left">
+                <ul className={ulStyle}>
+                  <li className={liStyle}>
+                    <NextLink href="/">
+                      <a className={linkStyle}>Home</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                </ul>
+                <ul className={ulStyle}>
+                  <li className={liStyle}>
+                    <NextLink href="/about">
+                      <a className={linkStyle}>About</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                  <li className={liStyle}>
+                    <NextLink href="/about-this-website">
+                      <a className={linkStyle}>About&nbsp;this&nbsp;website</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                  <li className={liStyle}>
+                    <NextLink href="/antilibrary">
+                      <a className={linkStyle}>Antilibrary</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                  <li className={liStyle}>
+                    <NextLink href="/blog">
+                      <a className={linkStyle}>Blog</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                  <li className={liStyle}>
+                    <NextLink href="/books">
+                      <a className={linkStyle}>Books</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                </ul>
+                <ul className={ulStyle}>
+                  <li className={liStyle}>
+                    <NextLink href="/favorites">
+                      <a className={linkStyle}>Favorites</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                  <li className={liStyle}>
+                    <NextLink href="/movies">
+                      <a className={linkStyle}>Movies</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                  <li className={liStyle}>
+                    <NextLink href="/now">
+                      <a className={linkStyle}>Now</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                  <li className={liStyle}>
+                    <NextLink href="/portfolio">
+                      <a className={linkStyle}>Portfolio</a>
+                    </NextLink>
+                    {/* {pageSeparator} */}
+                  </li>
+                  <li className={liStyle}>
+                    <NextLink href="/recipe-book">
+                      <a className={linkStyle}>Recipe&nbsp;Book</a>
+                    </NextLink>
+                  </li>
+                </ul>
+              </nav>
             </div>
           </div>
         </motion.div>
