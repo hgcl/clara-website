95553c8e1d4fca2b4af78c3c0da0708f7fc6b730
hgcl <hey@clarale.com>
2021-09-09 17:35:47 +0200

    :recycle: Remove DropdownButton component from antilibrary

--- pages/antilibrary.js
+++ pages/antilibrary.js
@@ -4,19 +4,19 @@ import Layout from "../components/layout";
 import Header from "../components/header";
 import Container from "../components/container";
 import MediaGrid from "../components/MediaGrid";
-import { getYear } from "date-fns";
+// import { getYear } from "date-fns";
 import { useState } from "react";
-import DropdownButton from "../components/DropdownButton";
+// import DropdownButton from "../components/DropdownButton";
 import MediaTile from "../components/MediaTile";
 
 export default function Antilibrary({ allBooks }) {
   // Year media dropdown:
-  const [yearFilter, setYear] = useState(0);
-  const yearDropdown = [
-    { name: "All", value: 0 },
-    { name: "2021", value: 2021 },
-    { name: "2020", value: 2020 },
-  ];
+  // const [yearFilter, setYear] = useState(0);
+  // const yearDropdown = [
+  //   { name: "All", value: 0 },
+  //   { name: "2021", value: 2021 },
+  //   { name: "2020", value: 2020 },
+  // ];
   return (
     <Layout title={"Antilibrary"}>
       <Container>
@@ -26,20 +26,20 @@ export default function Antilibrary({ allBooks }) {
           }
         />
         <div className="flex flex-col sm:flex-row flex-wrap">
-          <DropdownButton
+          {/* <DropdownButton
             title="Year"
             filter={yearFilter}
             setHook={setYear}
             itemsArray={yearDropdown}
             marginRight="mr-2"
-          />
+          /> */}
         </div>
         <MediaGrid>
           {antilibraryData
             // Year filter
-            .filter((book) =>
-              yearFilter ? getYear(new Date(book.date)) === yearFilter : book
-            )
+            // .filter((book) =>
+            //   yearFilter ? getYear(new Date(book.date)) === yearFilter : book
+            // )
             .sort((a, b) => new Date(b.date) - new Date(a.date))
             .map((book) => {
               const {
