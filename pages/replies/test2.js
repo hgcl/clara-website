90b187c93934a17127b5bc13fcf9c21ead2431ca
hgcl <hey@clarale.com>
2021-09-10 15:02:16 +0200

    :recycle: Fix lint errors

--- pages/replies/test2.js
+++ pages/replies/test2.js
@@ -9,16 +9,18 @@ export default function Test2() {
         <p>
           in reply to:
           <a
-            class="u-in-reply-to"
+            className="u-in-reply-to"
             href="https://aaronparecki.com/2018/06/30/11/your-first-webmention"
           >
             @aaronpk
           </a>
         </p>
-        <p class="e-content">Trying out this guide to sending webmentions</p>
+        <p className="e-content">
+          Trying out this guide to sending webmentions
+        </p>
         <p>
-          <a href="https://aaronpk.com/reply.html" class="u-url">
-            <time class="dt-published" datetime="2021-01-07 19:28:36">
+          <a href="https://aaronpk.com/reply.html" className="u-url">
+            <time className="dt-published" dateTime="2021-01-07 19:28:36">
               January 7, 2021
             </time>
           </a>
