b92cfdcb65256921aa42c4b6d2eb17c4a9ed699e
hgcl <hey@clarale.com>
2021-09-12 17:27:17 +0200

    :bug: ESlint Fix a few anchor links

--- components/header.js
+++ components/header.js
@@ -1,13 +1,11 @@
-import Link from "next/link";
+import Link from "./link";
 import { WEBSITE_NAME } from "../lib/constants";
 
 export default function Header({ pageDescription }) {
   return (
     <>
       <h2 className="text-2xl md:text-4xl tracking-tight md:tracking-tighter leading-tight pt-2 md:pt-4 mt-8">
-        <Link href="/">
-          <a>{WEBSITE_NAME}</a>
-        </Link>
+        <Link href="/">{WEBSITE_NAME}</Link>
       </h2>
       <p className="comment tracking mb-20 ml-1">{pageDescription}</p>
     </>
--- components/hero-post.js
+++ components/hero-post.js
@@ -1,14 +1,13 @@
-// import Avatar from "../components/avatar";
 import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
-import Link from "next/link";
+import Link from "./link";
 
 export default function HeroPost({
   title,
   coverImage,
   date,
   excerpt,
-  author,
+  // author,
   slug,
 }) {
   return (
@@ -20,7 +19,7 @@ export default function HeroPost({
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
             <Link as={`/blog/${slug}`} href="/blog/[slug]">
-              <a className="hover:underline">{title}</a>
+              {title}
             </Link>
           </h3>
           <div className="mb-4 md:mb-0 text-lg">
@@ -29,7 +28,6 @@ export default function HeroPost({
         </div>
         <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          {/* <Avatar name={author.name} picture={author.picture} /> */}
         </div>
       </div>
     </section>
