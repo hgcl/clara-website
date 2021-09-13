3ee5e136162bcfbfd71d6b95a050514a6b9ab4ef
hgcl <hey@clarale.com>
2021-09-13 22:58:36 +0200

    :lipstick: Add base styles to app

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import flush from "styled-jsx/server";
 
 export default class MyDocument extends Document {
   render() {
+    const styles = flush();
     return (
       <Html lang="en">
         <Head>
+          {styles}
           <link
             rel="alternate"
             type="application/rss+xml"
