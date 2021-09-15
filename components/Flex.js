261da97538188c825f96b29f2c622ad2de87bd81
hgcl <hey@clarale.com>
2021-09-15 08:01:53 +0200

    :lipstick: Add screen breakpoints

--- components/Flex.js
+++ components/Flex.js
@@ -1,10 +1,24 @@
 export default function Flex({ children, earlyBreakpoint, className }) {
+  const styles = (
+    <style jsx>{`
+      .flex {
+        display: flex;
+        flex-direction: column;
+      }
+      @media screen and (min-width: 768px) {
+        .flex {
+          flex-direction: row;
+        }
+      }
+    `}</style>
+  );
   return (
     <div
       className={`flex flex-col ${
         earlyBreakpoint ? "lg:flex-row" : "md:flex-row"
       } ${className}`}
     >
+      {styles}
       {children}
     </div>
   );
