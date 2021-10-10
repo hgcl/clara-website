00168b94e81cc24cf1a5941b19d528de5163744a
hgcl <hey@clarale.com>
2021-10-10 10:39:34 +0200

    Remove old code from index

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,13 @@
 import Container from "../components/container";
-// import MoreStories from "../components/more-stories";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
-// import { getAllPosts } from "../lib/getAllPosts";
 
 export default function Index() {
-  // const latestPosts = allPosts.slice(0, 3);
   return (
     <Layout noHentry noBottomMargin>
       <Container>
         <Intro />
-        {/* {allPosts.length > 0 && <MoreStories posts={latestPosts} />} */}
       </Container>
     </Layout>
   );
 }
-
-// export async function getStaticProps() {
-//   const allPosts = getAllPosts([
-//     "title",
-//     "date",
-//     "slug",
-//     "excerpt",
-//   ]);
-
-//   return {
-//     props: { allPosts },
-//   };
-// }
