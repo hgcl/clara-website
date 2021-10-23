5456216c84cbb4a565ce7710afba346a4088563e
hgcl <hey@clarale.com>
2021-10-23 19:52:34 +0200

    Remove TailwindCSS code

--- components/more-stories.js
+++ components/more-stories.js
@@ -2,11 +2,9 @@ import PostPreview from "./post-preview";
 
 export default function MoreStories({ posts }) {
   return (
-    <section className="lg:flex">
-      <h2 className="flex-auto mr-16 text-6xl md:text-7xl tracking-tighter leading-tight">
-        Here&apos;s What&apos;s New
-      </h2>
-      <div className="flex-auto max-w-2xl mt-2">
+    <section>
+      <h2>Here&apos;s What&apos;s New</h2>
+      <div>
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
